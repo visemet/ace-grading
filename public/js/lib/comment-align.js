ff65848267192677450f74d885436a325e3923ea
Max Hirschhorn <maxh@caltech.edu>
2014-02-14 19:14:53 -0800

    Created utility to manage position of comments.
    Very naive approach. Meant to be used as follows.
    
        align = require('lib/align');
        align.move(index, $comments);
        align.above(index, $comments);
        align.below(index, $comments);
    
    This algorithm is greedy in the sense that when a comment is
    highlighted, it will appear next to the corresponding line of the code.
    So rather than maximizing the number of visible comments on the screen,
    we choose to display the comment as close as possible to the content it
    refers to.
    
    Unsure as to whether we can say that if we reach a comment that does
    not need to move, then we can break out of the loop, i.e. while
    adjusting the position of the comments above, upon reaching a comment
    that does not need to be moved, would comments prior to that one still
    need to be adjusted, or no?

---
+++ public/js/lib/comment-align.js
@@ -0,0 +1,73 @@
+
+require.config({
+  baseUrl: '/js',
+  paths: {
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min'
+  }
+});
+
+define(function(require, exports, module) {
+  'use strict';
+
+  /**
+   * Module dependencies.
+   */
+
+  var $ = require('jquery');
+
+  var PADDING = 12; // leave some space between comments
+
+  /**
+   * Moves the specified to comment to its preferred position.
+   */
+  exports.move = function(index, $comments) {
+    var $comment = $comments.eq(index);
+
+    var preferredTop = $comment.data('preferred-top')
+      , currentTop = parseInt($comment.css('top'), 10);
+
+    $comment.css('top', preferredTop);
+    return (!$.isNumeric(currentTop) || preferredTop !== currentTop);
+  };
+
+  /**
+   * Adjusts the position of all the comments above the specified
+   * comment by moving them as far down as possible, or as much they
+   * prefer.
+   */
+  exports.above = function(index, $comments) {
+    var $comment = $comments.eq(index)
+      , i;
+
+    var extent = parseInt($comment.css('top'), 10);
+    for (i = index - 1; i >= 0; i--) {
+      var $c = $comments.eq(i);
+      var preferredTop = $c.data('preferred-top');
+
+      // Move comment down as far as possible, or much as preferred
+      extent = Math.min(preferredTop, extent - $c.height() - PADDING);
+      $c.css('top', extent);
+    }
+  };
+
+  /**
+   * Adjusts the position of all the comments below the specified
+   * comment by moving them as far up as possible, or as much they
+   * prefer.
+   */
+  exports.below = function(index, $comments) {
+    var $comment = $comments.eq(index), $prev = $comment
+      , i, length = $comments.length;
+
+    var extent = parseInt($comment.css('top'), 10);
+    for (i = index + 1; i < length; i++) {
+      var $c = $comments.eq(i);
+      var preferredTop = $c.data('preferred-top');
+
+      // Move comment up as far as possible, or much as preferred
+      extent = Math.max(preferredTop, extent + $prev.height() + PADDING);
+      $c.css('top', extent);
+      $prev = $c;
+    }
+  };
+});
