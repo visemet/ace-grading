db9b31f831bedd2a97a6b55158a14deaaf600cb4
Max Hirschhorn <maxh@caltech.edu>
2014-02-19 11:00:03 -0800

    Created a "main" module for displaying comments.

---
+++ public/js/lib/comment-vm/main.js
@@ -0,0 +1,33 @@
+
+require.config({
+  baseUrl: '/js',
+  paths: {
+    ace: 'vendor/ace',
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min'
+  }
+});
+
+define(function(require, exports, module) {
+  'use strict';
+
+  /**
+   * Module dependencies.
+   */
+
+  var $ = require('jquery')
+    , ace = require('ace/ace');
+
+  var editor = ace.edit('editor')
+    , $comments = $('#comments');
+
+  var List = require('lib/comment/list').List
+    , Comment = require('lib/comment/comment').Comment;
+
+  var Environment = require('lib/comment-vm/show').Environment;
+
+  // TODO: Retrieve the list of comments
+  var comments = new List()
+    , env = new Environment(editor, comments);
+
+  env.display($comments);
+});
